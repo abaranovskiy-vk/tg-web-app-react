09f08cb34199f3535a83afd0389940f97eb0756f
a.baranovskiy <a.baranovskiy@a-baranovskiy.devpoint.i>
2023-10-25 17:54:55 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import './App.css';
+
+const tg = window.Telegram.WebApp;
+
+function App() {
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    function onClose () {
+        tg.close();
+    }
+
+  return (
+    <div className="App">
+        <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
